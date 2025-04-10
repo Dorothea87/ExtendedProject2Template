9ac4fe85d17ff98333b3ff6a18ea8afa8886058b
Dorothea Toeller <dorothea.toeller@mac.home>
2025-04-10 16:45:12 +0100

    add routes and application controler methods

---
+++ app/controllers/ApplicationController.scala
@@ -0,0 +1,16 @@
+package controllers
+
+import play.api.mvc.{BaseController, ControllerComponents}
+
+import javax.inject.Inject
+
+
+@Singleton
+class ApplicationController @Inject() (val controllerComponents: ControllerComponents) extends BaseController {
+
+def create() = TODO
+def read(username: String) = TODO
+def update(username: String) = TODO
+def delete(username: String) = TODO
+
+}
