e8123889200ccb8c9090f68aa33ff38a03b765e1
Nimo Mohamed <138133949+nimo-mohamed@users.noreply.github.com>
2025-04-11 15:05:45 +0100

    finish ApplicationController

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -1,8 +1,8 @@
 package controllers
 
 import model.DataModel
-import play.api.libs.json.{JsError, JsSuccess, JsValue}
-import play.api.mvc._
+import play.api.libs.json.{JsError, JsSuccess, JsValue, Json}
+import play.api.mvc.{Action, _}
 import service.RepositoryService
 
 import javax.inject.{Inject, Singleton}
@@ -10,7 +10,7 @@ import scala.concurrent.{ExecutionContext, Future}
 
 
 @Singleton
-class ApplicationController @Inject() (val controllerComponents: ControllerComponents, val repositoryService: RepositoryService)(implicit val ec: ExecutionContext) extends BaseController {
+class ApplicationController @Inject()(val controllerComponents: ControllerComponents, val repositoryService: RepositoryService)(implicit val ec: ExecutionContext) extends BaseController {
 
   def create(): Action[JsValue] = Action.async(parse.json) { implicit request =>
     request.body.validate[DataModel] match {
@@ -20,9 +20,25 @@ class ApplicationController @Inject() (val controllerComponents: ControllerCompo
     }
   }
 
+  def read(username: String): Action[AnyContent] = Action.async { implicit request =>
+    repositoryService.read(username).map(user => Ok {
+      Json.toJson(user)
+    })
+  }
 
-def read(username: String) = TODO
-def update(username: String) = TODO
-def delete(username: String) = TODO
+  def update(username: String): Action[JsValue] = Action.async(parse.json) { implicit request =>
+    request.body.validate[DataModel] match {
+      case JsSuccess(dataModel, _) =>
+        repositoryService.update(username, dataModel).map(_ => Accepted {
+          request.body
+        })
+      case JsError(_) => Future(BadRequest)
+    }
+  }
 
+
+  def delete(username: String): Action[AnyContent] = Action.async { implicit request =>
+    repositoryService.delete(username).map(_ => Accepted)
+
+  }
 }
