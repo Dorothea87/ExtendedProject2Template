e534734f04b19992f9d0a3f8c6660d3991b8e0d9
Eloise Holland <eloise.holland@Mac.Home>
2025-04-11 13:07:09 +0100

    application controller create method

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -1,18 +1,25 @@
 package controllers
 
-import play.api.libs.json.JsValue
-import play.api.mvc.{BaseController, ControllerComponents, Results}
+import model.DataModel
+import play.api.libs.json.{JsError, JsSuccess, JsValue}
+import play.api.mvc._
+import service.RepositoryService
 
 import javax.inject.{Inject, Singleton}
+import scala.concurrent.{ExecutionContext, Future}
 
 
 @Singleton
-class ApplicationController @Inject() (val controllerComponents: ControllerComponents) extends BaseController {
+class ApplicationController @Inject() (val controllerComponents: ControllerComponents, val repositoryService: RepositoryService)(implicit val ec: ExecutionContext) extends BaseController {
 
-def create(): Action[JsValue] = Action.async(){ implicit request =>
- body.validate[DataModel] match {
-   case Created(200 -> OK)
- }}
+  def create(): Action[JsValue] = Action.async(parse.json) { implicit request =>
+    request.body.validate[DataModel] match {
+      case JsSuccess(dataModel, _) =>
+        repositoryService.create(dataModel).map(_ => Created)
+      case JsError(_) => Future(BadRequest)
+    }
+  }
+
 
 def read(username: String) = TODO
 def update(username: String) = TODO
