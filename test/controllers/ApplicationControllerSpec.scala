b55748d9535edffde76b8cdbfe357ff1abdb2a19
Dorothea Toeller <dorothea.toeller@mac.home>
2025-04-14 11:43:05 +0100

    layout teststructure in app controller spec and basespec

--- test/controllers/ApplicationControllerSpec.scala
+++ test/controllers/ApplicationControllerSpec.scala
@@ -1,9 +1,14 @@
 package controllers
 
 import baseSpec.BaseSpecWithApplication
+import model.DataModel
 import play.api.test.FakeRequest
 import play.api.http.Status
+import play.api.libs.json.{JsValue, Json}
+import play.api.mvc.Result
 import play.api.test.Helpers._
+
+import scala.concurrent.Future
 
 
 class ApplicationControllerSpec extends BaseSpecWithApplication {
@@ -11,8 +16,33 @@ class ApplicationControllerSpec extends BaseSpecWithApplication {
   val TestApplicationController = new ApplicationController(
     component, repoService
   )(executionContext)
-  "ApplicationController .create()" should {
-    val result = TestApplicationController.create()(FakeRequest())
+
+  private val dataModel: DataModel = DataModel(
+    "abcd",
+    "test name",
+    "test date",
+    "London",
+    100,
+    100
+  )
+
+  //
+  //  "ApplicationController .create()" should {
+  //    val result = TestApplicationController.create()(FakeRequest())
+  //
+  //  }
+
+  "ApplicationController .create" should {
+
+    "create a user in the database" in {
+      beforeEach()
+      val request: FakeRequest[JsValue] = buildPost("/api").withBody[JsValue](Json.toJson(dataModel))
+      val createdResult: Future[Result] = TestApplicationController.create()(request)
+
+      status(createdResult) shouldBe Status.OK
+      afterEach()
+    }
+
 
   }
 
@@ -30,5 +60,10 @@ class ApplicationControllerSpec extends BaseSpecWithApplication {
   "ApplicationController .delete(username: String)" should {
 
   }
+
+
+  override def beforeEach(): Unit = await(repository.deleteAll())
+
+  override def afterEach(): Unit = await(repository.deleteAll())
 
 }
