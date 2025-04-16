ed447b3fc07dd129dd5d2dc044c63ef4f5364063
Dorothea Toeller <dorothea.toeller@mac.home>
2025-04-16 15:27:00 +0100

    refactor github connector with EitherT

--- app/connectors/GithubConnector.scala
+++ app/connectors/GithubConnector.scala
@@ -1,18 +1,31 @@
 package connectors
 
-import play.api.libs.json.OFormat
-import play.api.libs.ws.WSClient
+import cats.data.EitherT
+import cats.data.EitherT.rightT
+import com.google.inject._
+import models.{APIError, GitHubUser}
+import play.api.libs.json.{JsError, JsSuccess, OFormat, Reads}
+import play.api.libs.ws.{WSClient, WSResponse}
 
-import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
 
+@Singleton
 class GithubConnector @Inject()(ws: WSClient) {
-  def get[Response](url: String)(implicit rds: OFormat[Response], ec: ExecutionContext): Future[Response] = {
+
+  def get[Response](url: String)(implicit rds: Reads[Response], ec: ExecutionContext): EitherT[Future, APIError, Response] = {
     val request = ws.url(url)
     val response = request.get()
-    response.map {
-      result =>
-        result.json.as[Response]
+
+    EitherT {
+      response.map { result =>
+        if (result.status == 200) {
+          Right(result.json.as[Response])
+        } else {
+          Left(APIError.BadAPIResponse(result.status, result.statusText))
+        }
+      }.recover { case _: WSResponse =>
+        Left(APIError.BadAPIResponse(500, "Could not connect to API."))
+      }
     }
   }
 }
--- app/services/GitHubService.scala
+++ app/services/GitHubService.scala
@@ -1,13 +1,15 @@
 package services
 
+import cats.data.EitherT
 import connectors.GithubConnector
-import models.{DataModel, GitHubUser}
+import models.{APIError, DataModel, GitHubUser}
 
 import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
 
 class GitHubService @Inject()(connector: GithubConnector) {
 
-  def getGithubUserByName(urlOverride: Option[String] = None, username: String) (implicit ec: ExecutionContext): Future[GitHubUser] =
-    connector.get[DataModel](urlOverride.getOrElse(s"https://api.github.com/users/$username"))
+  def getGithubUserByName(urlOverride: Option[String] = None, username: String)(implicit ec: ExecutionContext): EitherT[Future, APIError, GitHubUser] = {
+    connector.get[GitHubUser](urlOverride.getOrElse(s"https://api.github.com/users/$username"))
+  }
 }
